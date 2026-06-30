093d42bc50709e98b81f5e23f5e480ed2792b62b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-06-30 15:21:23 +0200

    Revert "fix(emitModels): handle folded players correctly when showing hands"
    This reverts commit 440319f3e58f013016df46d00fb801e92107b355.

--- models/emitModels.py
+++ models/emitModels.py
@@ -51,9 +51,7 @@ class EmitPlayer:
             is_folded=player.is_folded,
             is_out=player.is_out,
             is_round_ready=player.is_round_ready,
-            hand=EmitHand.from_player(player)
-            if show_hand and not player.is_folded
-            else None,
+            hand=EmitHand.from_player(player) if show_hand else None,
         )
 
 
